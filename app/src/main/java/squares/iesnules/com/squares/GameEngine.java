9506cd7f74614a39344fef4552c04217203e0281
Rafa <rubio_rafmor@gva.es>
2015-03-03 19:20:41 +0100

    GameEngine basic interface definition.

--- app/src/main/java/squares/iesnules/com/squares/GameEngine.java
+++ app/src/main/java/squares/iesnules/com/squares/GameEngine.java
@@ -5,4 +5,37 @@ package squares.iesnules.com.squares;
  */
 public class GameEngine {
 
+    private static int MAX_ROWS = 100;
+    private static int MAX_COLS = 100;
+
+    private byte[][] mGameState;
+
+    public GameEngine(int rows, int cols) throws RuntimeException {
+        if (rows > 0 && cols > 0) {
+            mGameState = generateGameState(rows, cols);
+        }
+        else {
+            throw new RuntimeException("GameEngine: Invalid board dimensions");
+        }
+    }
+
+    public GameEngine(byte[][] newState ) {
+        if (checkGameStateSanity(newState)) {
+            mGameState = newState;
+        }
+        else {
+            throw new RuntimeException("GameEngine: Invalid game state");
+        }
+    }
+
+    private byte[][] generateGameState(int rows, int cols) {
+        // TODO: Generate an empty board state array
+        return new byte[1][1];
+    }
+
+    private boolean checkGameStateSanity(byte[][] state) {
+        // TODO: Check if game state represented by 'state' is valid
+        return false;
+    }
+
 }
