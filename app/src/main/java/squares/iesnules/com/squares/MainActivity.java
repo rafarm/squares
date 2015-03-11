240e23929e3508a5404b6af1f8c403df2d13a77b
alumne <rubenferrandis6997@gmail.com>
2015-03-11 10:50:31 +0100

    I have done one button that when you push it, appears a layaut with 2,3,4 players option.

--- app/src/main/java/squares/iesnules/com/squares/MainActivity.java
+++ app/src/main/java/squares/iesnules/com/squares/MainActivity.java
@@ -1,9 +1,13 @@
 package squares.iesnules.com.squares;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +39,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showOfflineOption(View view) {
+        LinearLayout optionsLayout = (LinearLayout)findViewById(R.id.optionsLayout);
+        optionsLayout.setVisibility(View.VISIBLE);
+        Button offline = (Button) findViewById(R.id.offlineButton);
+        offline.setVisibility(View.INVISIBLE);
     }
 }
