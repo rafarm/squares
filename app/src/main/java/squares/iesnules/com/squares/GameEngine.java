5fa5d44a9f2bb3e80cff0b0d84fd485e30c7d49d
neusroque <ne_ro_al@hotmail.com>
2015-03-11 10:23:15 +0100

    matriz inicial terminada

--- app/src/main/java/squares/iesnules/com/squares/GameEngine.java
+++ app/src/main/java/squares/iesnules/com/squares/GameEngine.java
@@ -28,9 +28,31 @@ public class GameEngine {
         }
     }
 
+    //This method is executed when a new game is started, showing the initial matrix,the valid and invalid values.
     private byte[][] generateGameState(int rows, int cols) {
-        // TODO: Generate an empty board state array
-        return new byte[1][1];
+
+        byte[][] matrix = new byte[rows][cols];
+
+        for(int j=0;j<100;j++){
+            for(int i=0;i<100;i++){
+                if(j%2!= 0){
+                    matrix[i][j] = 0;
+                    //if the result is 0, the player will can press in this
+                }
+                else{
+                    if (i%2!=0){
+                        matrix[i][j]=0;
+                        //if the result is 0, the player will can press in this
+                    }
+                    else{
+                        matrix[i][j]=-1;
+                        //if the result is -1,the player won't can press in this.The result -1 is an invalid value
+                    }
+                }
+            }
+        }
+
+        return matrix;
     }
 
     private boolean checkGameStateSanity(byte[][] state) {
