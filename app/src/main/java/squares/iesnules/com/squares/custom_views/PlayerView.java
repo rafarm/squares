76e60ae730745bb80e9fbe88e5a42fbac77be7c7
jordi <jordilengua@gmail.com>
2015-03-16 12:12:44 +0100

    programar en la pantalla de joc, la image i el nome de cada jugador

--- app/src/main/java/squares/iesnules/com/squares/custom_views/PlayerView.java
+++ app/src/main/java/squares/iesnules/com/squares/custom_views/PlayerView.java
@@ -9,7 +9,11 @@ import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import squares.iesnules.com.squares.R;
 
@@ -17,6 +21,8 @@ import squares.iesnules.com.squares.R;
  * TODO: document your custom view class.
  */
 public class PlayerView extends LinearLayout {
+    private ImageView mPlayerImage;
+    private TextView mPlayerName;
 
     public PlayerView(Context context) {
         super(context);
@@ -41,5 +47,21 @@ public class PlayerView extends LinearLayout {
         // TODO: Process AtributeSet
 
         a.recycle();
+
+        mPlayerImage = (ImageView)findViewById(R.id.playerImage);
+        mPlayerName = (TextView)findViewById(R.id.playerName);
     }
+
+    public Drawable getPlayerImage() {
+        return mPlayerImage.getDrawable():
+    }
+
+    public void setPlayerImage(Drawable image) {
+        mPlayerImage.setImageDrawable(image);
+    }
+    public Text getPlayerName(){
+        return mPlayerName.getText();
+    }
+    public void setPlayerName(Text text);
+       mPlayerName.set
 }
